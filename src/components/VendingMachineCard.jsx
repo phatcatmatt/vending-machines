dcb58685a5c1184005b154ecfd8796a3bde1bb81
Matt Nicholas <matt@mattnicholas.io>
2018-04-18 22:37:26 -0400

    styles for vending machine cards

--- src/components/VendingMachineCard.jsx
+++ src/components/VendingMachineCard.jsx
@@ -7,14 +7,19 @@ class VendingMachineCard extends Component {
     const { id: machineId, attributes, deleteCB } = this.props;
     return (
       <li className="vending-machine-card">
-        <h4>vending machine - {machineId}</h4>
-        <p>Lat: {attributes.latitude}</p>
-        <p>Lon: {attributes.longitude}</p>
-        <span
-          className="icon" 
-          onClick={() => deleteCB(machineId)}>
-          <i className="material-icons">delete</i>
-        </span>
+        <div className="header-row">
+        {/* TODO: add a sequential ID next to the machine so it says Vending Machine #123 */}
+          <h1>vending machine</h1>
+          <span
+            className="icon" 
+            onClick={() => deleteCB(machineId)}>
+            <i className="material-icons">delete</i>
+          </span>
+        </div>
+        <div className="machine-attributes">
+        {/* TODO: is this formatted right? Are they degrees with North and South etc? */}
+          <p>{attributes.latitude} - {attributes.longitude}</p>
+        </div>
       </li>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+//TODO: this reset is throwing a warning saying it's not being used but it is still working
 import reset from 'reset-css';
 import 'normalize.css';
 import './index.css';
